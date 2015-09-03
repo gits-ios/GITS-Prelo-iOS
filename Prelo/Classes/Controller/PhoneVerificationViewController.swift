d391d3906e5e6fda7f738f8b5a34a81d35cf5c7b
Fransiska <Fransiska@Fransiskas-MacBook-Air.local>
2015-09-03 13:07:00 +0700

    Phone verification new file

---
+++ Prelo/Classes/Controller/PhoneVerificationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhoneVerificationViewController.swift
+//  Prelo
+//
+//  Created by Fransiska on 9/2/15.
+//  Copyright (c) 2015 GITS Indonesia. All rights reserved.
+//
+
+import Foundation
+
+class PhoneVerificationViewController : BaseViewController {
+    
+}
