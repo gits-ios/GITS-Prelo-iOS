87cac9fbbd1fb23cffcafe6d6993724c818cc503
kumangkumeng <kumangxxx@gits.co.id>
2015-09-25 11:40:56 +0700

    api tarik tunai

--- Prelo/Classes/Controller/TarikTunaiController.swift
+++ Prelo/Classes/Controller/TarikTunaiController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class TarikTunaiController: BaseViewController {
+class TarikTunaiController: BaseViewController, UIScrollViewDelegate
+{
     
     @IBOutlet var txtNamaBank : UITextField!
     @IBOutlet var txtNomerRekening : UITextField!
@@ -18,6 +19,12 @@ class TarikTunaiController: BaseViewController {
     
     @IBOutlet var captionPreloBalance : UILabel!
     @IBOutlet var scrollView : UIScrollView!
+    
+    @IBOutlet var btnWithdraw : UIButton!
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        self.view.endEditing(true)
+    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -39,5 +46,72 @@ class TarikTunaiController: BaseViewController {
         super.viewWillDisappear(animated)
         
         self.an_unsubscribeKeyboard()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Tarik Uang"
+        
+        scrollView.delegate = self
+        
+        captionPreloBalance.text = "0"
+        
+        txtNamaBank.textAlignment = NSTextAlignment.Right
+        txtNomerRekening.textAlignment = NSTextAlignment.Right
+        
+        getBalance()
+    }
+    
+    func getBalance()
+    {
+        request(APIWallet.GetBalance).responseJSON { req, resp, res, err in
+            if (APIPrelo.validate(true, err: err, resp: resp))
+            {
+                let json = JSON(res!)
+                if let i = json["_data"].int
+                {
+                    let f = NSNumberFormatter()
+                    f.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+                    f.currencySymbol = ""
+                    f.locale = NSLocale(localeIdentifier: "id_ID")
+                    self.captionPreloBalance.text = f.stringFromNumber(NSNumber(integer: i))
+                }
+            } else
+            {
+                self.navigationController?.popViewControllerAnimated(true)
+            }
+            
+        }
+    }
+    
+    @IBAction func withdraw()
+    {
+        let amount = txtJumlah.text
+        let namaBank = txtNamaBank.text
+        let norek = txtNomerRekening.text
+        let namarek = txtNamaRekening.text
+        let pass = txtPassword.text
+        
+        self.btnWithdraw.enabled = false
+        
+        request(APIWallet.Withdraw(amount: amount, targetBank: namaBank, norek: norek, namarek: namarek, password: pass)).responseJSON { req, resp, res, err in
+            self.btnWithdraw.enabled = true
+            if (APIPrelo.validate(true, err: err, resp: resp))
+            {
+                let json = JSON(res!)
+                if let message = json["_message"].string
+                {
+                    UIAlertView.SimpleShow("Perhatian", message: message)
+                } else
+                {
+                    self.getBalance()
+                }
+            } else
+            {
+                
+            }
+            
+        }
     }
 }
