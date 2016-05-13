92c1be96bd78812bd161da34cc379d7dc3ea876e
mralghazali <alghazalimr@gmail.com>
2016-05-13 11:17:08 +0700

    :+1: Put back update app alert with ordering

--- Prelo/AppDelegate.swift
+++ Prelo/AppDelegate.swift
@@ -21,7 +21,7 @@ protocol LoadAppDataDelegate {
 }
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UIAlertViewDelegate {
 
     var window: UIWindow?
 
@@ -759,6 +759,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
     }
     
+    // MARK: - UIAlertViewDelegate functions
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        switch buttonIndex {
+        case 0: // Update
+            UIApplication.sharedApplication().openURL(NSURL(string: "itms-apps://itunes.apple.com/id/app/prelo/id1027248488")!)
+            break
+        case 1: // Cancel
+            break
+        default:
+            break
+        }
+    }
+    
     // MARK: - Version check
     
     func versionCheck() {
@@ -849,11 +863,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // Check if app need to be updated
                 if let installedVer = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String {
                     if let newVer = CDVersion.getOne()?.appVersion {
-                        if (installedVer != newVer) {
+                        if (newVer.compare(installedVer, options: .NumericSearch, range: nil, locale: nil) == .OrderedDescending) {
                             let a = UIAlertView()
                             a.title = "New Version Available"
                             a.message = "Prelo \(newVer) is available on App Store"
-                            a.addButtonWithTitle("OK")
+                            a.addButtonWithTitle("Update")
+                            if let isForceUpdate = data["is_force_update"].bool {
+                                if (!isForceUpdate) {
+                                    a.addButtonWithTitle("Cancel")
+                                }
+                            } else {
+                                a.addButtonWithTitle("Cancel")
+                            }
+                            a.delegate = self
+
                             a.show()
                         }
                     }
