a7d0080e8934690c966a386f4233c514e92f61e3
mralghazali <alghazalimr@gmail.com>
2016-07-28 17:42:55 +0700

    :+1: Setup account alert text lil fix

--- Prelo/Classes/Controller/ProfileSetupViewController.swift
+++ Prelo/Classes/Controller/ProfileSetupViewController.swift
@@ -197,7 +197,7 @@ class ProfileSetupViewController : BaseViewController, PickerViewDelegate, UINav
     }
     
     override func backPressed(sender: UIBarButtonItem) {
-        let alert : UIAlertController = UIAlertController(title: "Perhatian", message: "Setelan akun belum selesai. Halaman ini akan muncul lagi lain kali kamu login. Keluar?", preferredStyle: UIAlertControllerStyle.Alert)
+        let alert : UIAlertController = UIAlertController(title: "Perhatian", message: "Setelan akun belum selesai. Halaman ini akan muncul lagi ketika kamu login. Keluar?", preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Batal", style: .Default, handler: nil))
         alert.addAction(UIAlertAction(title: "Keluar", style: .Default, handler: { action in
             User.Logout()
