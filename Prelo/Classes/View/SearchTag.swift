5a86876a6f86707be6d46d90a10ab2285945da37
Djuned Fernando Djusdek <djuned@prelo.id>
2017-01-03 22:27:53 +0700

    hot fix
    — top search / history if text is very long

--- Prelo/Classes/View/SearchTag.swift
+++ Prelo/Classes/View/SearchTag.swift
@@ -28,7 +28,7 @@ class SearchTag: BorderedView {
         s.captionHide.text = tagString
         s.captionHide.sizeToFit()
         
-        s.bounds = CGRect(x: 0, y: 0, width: s.captionHide.width+16, height: s.captionHide.height+8)
+        s.bounds = CGRect(x: 0, y: 0, width: s.captionHide.width+16 <= UIScreen.main.bounds.width-16 ? s.captionHide.width+16 : UIScreen.main.bounds.width-16, height: s.captionHide.height+8)
         
         s.layer.cornerRadius = s.height/2
         s.layer.masksToBounds = true
