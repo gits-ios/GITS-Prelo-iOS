9643599e62462e3c25f3da3f767e9deaf7e79fb3
Djuned Fernando Djusdek <djuned@prelo.id>
2017-04-13 15:10:10 +0700

    preparing vc
    — payment method

--- Prelo/Classes/Controller/Checkout2PayViewController.swift
+++ Prelo/Classes/Controller/Checkout2PayViewController.swift
@@ -13,3 +13,174 @@ import Foundation
 class Checkout2PayViewController: BaseViewController {
     
 }
+
+// MARK: - Class Checkout2PaymentMethodCell
+class Checkout2PaymentMethodCell: UITableViewCell {
+    @IBOutlet weak var lbTitle: UILabel!
+    
+    func adapt(_ title: String) { // Metode / Ringkasan Pembayaran
+        self.lbTitle.text = title
+    }
+    
+    static func heightFor() -> CGFloat {
+        return 52.0
+    }
+}
+
+// MARK: - Class Checkout2PaymentBankCell
+class Checkout2PaymentBankCell: UITableViewCell {
+    @IBOutlet weak var lbCheck: UILabel!
+    @IBOutlet weak var lbBank: UILabel!
+    @IBOutlet weak var lbDropdown: UILabel!
+    
+    func adapt(_ bankName: String?, isSelected: Bool) {
+        if let bn = bankName {
+            self.lbBank.text = bn
+        } else {
+            self.lbBank.text = "Pilih Bank Tujuan Transfer"
+        }
+        
+        if isSelected {
+            self.lbCheck.isHidden = false
+        } else {
+            self.lbCheck.isHidden = true
+        }
+    }
+    
+    static func heightFor(_ isSelected: Bool) -> CGFloat {
+        if isSelected {
+            return 99.0
+        }
+        return 35.0
+    }
+}
+
+// MARK: - Class Checkout2PaymentCreditCardCell
+class Checkout2PaymentCreditCardCell: UITableViewCell {
+    @IBOutlet weak var lbCheck: UILabel!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbDescription: UILabel!
+    
+    // Kartu Kredit
+    // Indomaret
+    // Mandiri Clickpay
+    // Kredivo
+    
+    func adapt(_ paymentMethodName: String, isSelected: Bool) {
+        self.lbTitle.text = paymentMethodName
+        self.lbDescription.text = "Pembayaran Aman dengan " + paymentMethodName
+        
+        if isSelected {
+            self.lbCheck.isHidden = false
+        } else {
+            self.lbCheck.isHidden = true
+        }
+    }
+    
+    static func heightFor(_ isSelected: Bool) -> CGFloat {
+        if isSelected {
+            return 62.0
+        }
+        return 35.0
+    }
+}
+
+// MARK: - Class Checkout2BlackWhiteCell
+class Checkout2BlackWhiteCell: UITableViewCell {
+    static func heightFor() -> CGFloat {
+        return 15.0
+    }
+}
+
+// MARK: - Class Checkout2PreloBalanceCell
+class Checkout2PreloBalanceCell: UITableViewCell {
+    @IBOutlet weak var btnSwitch: UISwitch!
+    @IBOutlet weak var txtInputPreloBalance: UITextField!
+    @IBOutlet weak var lbDescription: UILabel!
+    
+    var preloBalanceUsed: ()->() = {}
+    
+    func adapt(_ preloBalanceUsed: Int, preloBalanceTotal: Int, isUsed: Bool) {
+        self.txtInputPreloBalance.text = preloBalanceUsed.string
+        self.lbDescription.text = "Prelo Balance kamu" + preloBalanceTotal.asPrice
+        self.btnSwitch.isSelected = isUsed
+    }
+    
+    static func heightFor(_ isUsed: Bool) -> CGFloat {
+        if isUsed {
+            return 111.0
+        }
+        return 51.0
+    }
+    
+    @IBAction func btnSwitchPressed(_ sender: Any) {
+        self.preloBalanceUsed()
+    }
+}
+
+// MARK: - Class Checkout2VoucherCell
+class Checkout2VoucherCell: UITableViewCell {
+    @IBOutlet weak var btnSwitch: UISwitch!
+    @IBOutlet weak var txtInputVoucher: UITextField!
+    
+    var voucherUsed: ()->() = {}
+    var voucherApply: (String)->() = {_ in }
+    
+    func adapt(_ voucher: String, isUsed: Bool) {
+        self.txtInputVoucher.text = voucher
+        self.btnSwitch.isSelected = isUsed
+    }
+    
+    static func heightFor(_ isUsed: Bool) -> CGFloat {
+        if isUsed {
+            return 111.0
+        }
+        return 51.0
+    }
+    
+    @IBAction func btnSwitchPressed(_ sender: Any) {
+        self.voucherUsed()
+    }
+    
+    @IBAction func btnApplyPressed(_ sender: Any) {
+        if (self.txtInputVoucher.text != "") {
+            self.voucherApply(self.txtInputVoucher.text!)
+        }
+    }
+}
+
+// MARK: - Class Checkout2PaymentSummaryCell
+class Checkout2PaymentSummaryCell: UITableViewCell {
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbAmount: UILabel!
+    
+    func adapt(_ title: String, amount: Int) {
+        self.lbTitle.text = title
+        self.lbAmount.text = amount.asPrice
+    }
+    
+    static func heightFor() -> CGFloat {
+        return 24.0
+    }
+}
+
+// MARK: - Class Checkout2PaymentSummaryTotalCell
+class Checkout2PaymentSummaryTotalCell: UITableViewCell {
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbAmount: UILabel!
+    
+    var checkout: ()->() = {}
+    
+    func adapt(_ amount: Int) {
+        self.lbTitle.text = "Total Pembayaran"
+        self.lbAmount.text = amount.asPrice
+    }
+    
+    static func heightFor() -> CGFloat {
+        return 104.0
+    }
+    
+    @IBAction func btnCheckoutPressed(_ sender: Any) {
+        self.checkout()
+    }
+}
