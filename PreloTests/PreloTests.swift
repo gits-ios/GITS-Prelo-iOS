8fd844ede414ded4610ca3b02fd613d1c23ddfd4
ChyntiaLeonie <chyn_leo038@yahoo.co.id>
2017-06-07 12:57:43 +0700

    UITest for achievement, requestBarang, requestPackaging, tarikUang, referralBonus, testBantuan and Unit Test for achievement, login, and logout

--- PreloTests/PreloTests.swift
+++ PreloTests/PreloTests.swift
@@ -2,12 +2,12 @@
 //  PreloTests.swift
 //  PreloTests
 //
-//  Created by Rahadian Kumang on 7/6/15.
-//  Copyright (c) 2015 PT Kleo Appara Indonesia. All rights reserved.
+//  Created by Prelo on 6/7/17.
+//  Copyright © 2017 PT Kleo Appara Indonesia. All rights reserved.
 //
 
-import UIKit
 import XCTest
+
 
 class PreloTests: XCTestCase {
     
@@ -21,16 +21,225 @@ class PreloTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
+    var token = ""
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testAchievement() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+//            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+               
+//                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+//                print("ini isi data")
+//                print(isiData)
+                self.token = isiData["token"] as! String
+//                print("ini isi token")
+//                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/me/achievements")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "GET"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
         }
     }
+    
+    func testLogin(){
+        let username_or_email = "apaajaada"
+        let password = "password"
+
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+    }
+    
+    func testLogout() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            //            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+                
+                //                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+                //                print("ini isi data")
+                //                print(isiData)
+                self.token = isiData["token"] as! String
+                //                print("ini isi token")
+                //                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+            
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/auth/logout")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "POST"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+    }
+    
     
 }
--- PreloUITests/PreloUITests.swift
+++ PreloUITests/PreloUITests.swift
@@ -28,9 +28,122 @@ class PreloUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testAchievement() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //        let app = XCUIApplication()
+        //        app.buttons["MY ACCOUNT"].tap()
+        //
+        //        let tablesQuery2 = app.tables
+        //        let tablesQuery = tablesQuery2
+        //        tablesQuery.staticTexts["Achievement"].tap()
+        //        tablesQuery2.cells.containing(.staticText, identifier:"4 Poin").staticTexts[""].tap()
+        //        tablesQuery.staticTexts["Kumpulkan Poin untuk digunakan di aplikasi Prelo. Saat ini Poin sudah dapat digunakan untuk meng-up barang secara gratis!"].tap()
+        //        tablesQuery2.children(matching: .cell).element(boundBy: 9).staticTexts["The Ultimate Inspector"].swipeRight()
+        //        tablesQuery2.cells.containing(.staticText, identifier:"The Authentic Club").staticTexts[""].tap()
+        //        tablesQuery.staticTexts["Badge ini hanya untuk Prelovers yang selalu meng-upload barang original!"].tap()
+        //        tablesQuery2.children(matching: .cell).element(boundBy: 15).children(matching: .staticText).element.swipeRight()
+        //        tablesQuery2.cells.containing(.staticText, identifier:"Top Seller").staticTexts[""].tap()
+        
+        
+        let app = XCUIApplication()
+        app.buttons["MY ACCOUNT"].tap()
+        
+        let tablesQuery2 = app.tables
+        let tablesQuery = tablesQuery2
+        tablesQuery.staticTexts["Achievement"].tap()
+        tablesQuery2.cells.containing(.staticText, identifier:"4 Poin").staticTexts[""].tap()
+        tablesQuery.staticTexts["Kumpulkan Poin untuk digunakan di aplikasi Prelo. Saat ini Poin sudah dapat digunakan untuk meng-up barang secara gratis!"].tap()
+        tablesQuery.staticTexts[""].tap()
+        tablesQuery2.cells.containing(.staticText, identifier:"The Authentic Club").staticTexts[""].tap()
+        tablesQuery.staticTexts["Badge ini hanya untuk Prelovers yang selalu meng-upload barang original!"].tap()
+        tablesQuery2.cells.containing(.staticText, identifier:"Top Seller").staticTexts[""].tap()
+        tablesQuery.staticTexts["Khusus untuk 10 besar penjual yang menjual barang terbanyak setiap bulannya, rajin membalas chat, dan mendapatkan review yang bagus."].tap()
     }
+    
+    func testRequestBarang(){
+        
+        let app = XCUIApplication()
+        app.buttons["MY ACCOUNT"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Request"].tap()
+        tablesQuery.staticTexts["Request Barang"].tap()
+        app.webViews.staticTexts["REQUEST BARANG"].tap()
+        
+    }
+    
+    func testRequestPackaging(){
+        
+        let app = XCUIApplication()
+        app.buttons["MY ACCOUNT"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Request"].tap()
+        tablesQuery.staticTexts["Request Packaging"].tap()
+        app.webViews.staticTexts["REQUEST PACKAGING"].tap()
+        
+    }
+    
+    func testTarikUang(){
+        
+        let app = XCUIApplication()
+        app.buttons["MY ACCOUNT"].tap()
+        app.tables.staticTexts["Tarik Uang"].tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .staticText).matching(identifier: "Prelo Balance").element(boundBy: 0).tap()
+        app.buttons["TARIK UANG"].tap()
+        
+        let scrollViewsQuery = app.scrollViews
+        let elementsQuery = scrollViewsQuery.otherElements
+        elementsQuery.staticTexts["Prelo Balance yang bisa ditarik"].tap()
+        scrollViewsQuery.otherElements.containing(.staticText, identifier:"Prelo Balance yang bisa ditarik").children(matching: .other).element(boundBy: 2).children(matching: .button).element.tap()
+        app.otherElements.containing(.sheet, identifier:"Pilih Bank").children(matching: .other).element(boundBy: 0).tap()
+        elementsQuery.staticTexts["Nomor Rekening"].swipeRight()
+        elementsQuery.staticTexts["Rekening Atas Nama"].swipeUp()
+        elementsQuery.staticTexts["Jumlah Penarikan"].tap()
+        elementsQuery.staticTexts["Password Prelo"].tap()
+        elementsQuery.staticTexts["Penarikan dana ke rekening selain Mandiri, BCA, dan BNI, akan dikenakan biaya Rp6.500 dan akan dikenakan biaya Rp1.000 ke rekening BRI."].tap()
+        elementsQuery.buttons["TARIK UANG SEKARANG"].tap()
+        
+    }
+    
+    func testReferralBonus(){
+        
+        let app = XCUIApplication()
+        app.buttons["MY ACCOUNT"].tap()
+        
+        app.tables.staticTexts["Referral Bonus"].tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.staticTexts["SALDO REFERRAL BONUS"].tap()
+        elementsQuery.staticTexts["Untuk setiap referral yang dipakai, temanmu akan mendapatkan referral bonus Rp25.000 dan kamu akan mendapatkan Rp5.000. Dapatkan tambahan Rp20.000 saat temanmu berhasil belanja."].tap()
+        
+        let penggunaBaruHanyaDapatMemasukkanKodeReferralSebanyakSatuKaliStaticText = elementsQuery.staticTexts["(Pengguna baru hanya dapat memasukkan kode referral sebanyak satu kali)"]
+        penggunaBaruHanyaDapatMemasukkanKodeReferralSebanyakSatuKaliStaticText.tap()
+        penggunaBaruHanyaDapatMemasukkanKodeReferralSebanyakSatuKaliStaticText.swipeUp()
+        
+    }
+    
+    func testBantuan(){
+        
+        let app = XCUIApplication()
+        app.buttons["MY ACCOUNT"].tap()
+        
+        let app2 = app
+        app2.tables.staticTexts["Bantuan"].tap()
+        
+        let webViewsQuery = app2.webViews
+        webViewsQuery.staticTexts["Frequently Asked Questions"].tap()
+        
+        let webViewsQuery2 = app.webViews
+        let element = webViewsQuery2.children(matching: .other).element
+        element.tap()
+        
+        let frequentlyAnsweredQuestionPreloCoIdElement = webViewsQuery2.otherElements["Frequently Answered Question | Prelo.co.id"]
+        
+    }
+    
+    
     
 }
