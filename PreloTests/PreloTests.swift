895afa31e4d5f3e6270d07a2295356e2872eab86
ChyntiaLeonie <chyn_leo038@yahoo.co.id>
2017-06-07 14:42:44 +0700

    Unit Testing API for get unread notifications, get notifications, socmed data, remove all from cart, and get inbox threads

--- PreloTests/PreloTests.swift
+++ PreloTests/PreloTests.swift
@@ -496,4 +496,439 @@ class PreloTests: XCTestCase {
         }
     }
     
+    func testGetUnreadNotifications() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            //            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+                
+                //                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+                //                print("ini isi data")
+                //                print(isiData)
+                self.token = isiData["token"] as! String
+                //                print("ini isi token")
+                //                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+            
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/notif/unread")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "GET"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+    }
+    
+    
+    func testGetNotifications() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            //            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+                
+                //                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+                //                print("ini isi data")
+                //                print(isiData)
+                self.token = isiData["token"] as! String
+                //                print("ini isi token")
+                //                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+            
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/notification")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "GET"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+    }
+    
+    func testGetSocmedData() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            //            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+                
+                //                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+                //                print("ini isi data")
+                //                print(isiData)
+                self.token = isiData["token"] as! String
+                //                print("ini isi token")
+                //                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+            
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/socmed")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "GET"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+    }
+    
+    func testRemoveAllFromCart() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            //            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+                
+                //                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+                //                print("ini isi data")
+                //                print(isiData)
+                self.token = isiData["token"] as! String
+                //                print("ini isi token")
+                //                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+            
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/cart/remove_all")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "POST"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+    }
+    
+    func testGetInboxThreads() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //login buat dapet authorization
+        let username_or_email = "apaajaada"
+        let password = "password"
+        
+        let URL = NSURL(string: "https://dev.prelo.id/api/auth/login")!
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        var request = URLRequest(url:URL as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8)
+        
+        let expect = expectation(description: "GET \(URL)")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            //            print("cobain")
+            
+            if let convertedJsonIntoDict = try? JSONSerialization.jsonObject(with: data!, options: []){
+                
+                // Print out dictionary
+                
+                //                print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let tempDic = convertedJsonIntoDict as! Dictionary<String,Any>
+                let isiData = tempDic["_data"]! as! Dictionary<String,Any>
+                //                print("ini isi data")
+                //                print(isiData)
+                self.token = isiData["token"] as! String
+                //                print("ini isi token")
+                //                print(self.token)
+            } else {
+                print("masuknya kesini")
+            }
+            
+            
+            expect.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        let URL2 = NSURL(string: "https://dev.prelo.id/api/inbox")!
+        var request2 = URLRequest(url:URL2 as URL)
+        request2.httpMethod = "GET"
+        request2.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let expect2 = expectation(description: "GET \(URL2)")
+        let session2 = URLSession.shared
+        let task2 = session2.dataTask(with: request2 as URLRequest) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, URL2.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code 200")
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expect2.fulfill()
+        }
+        
+        task2.resume()
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+    }
 }
