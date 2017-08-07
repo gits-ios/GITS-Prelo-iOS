26d0dce24fa9840f2e4e7dcd28dccf69ef0214e7
Djuned Fernando Djusdek <djuned@prelo.id>
2017-08-07 10:43:04 +0700

    integration
    — define / connecting boutlet to vc

--- Prelo/Classes/Controller/AddProductViewController3.swift
+++ Prelo/Classes/Controller/AddProductViewController3.swift
@@ -18,13 +18,33 @@ class AddProductViewController3: BaseViewController {
 
 // MARK: - Title Cell
 class AddProduct3ImageTitleCell: UITableViewCell {
-    @IBOutlet weak var SectionImage: UIImageView!
+    @IBOutlet weak var SectionImage: TintedImageView!
     @IBOutlet weak var SectionTitle: UILabel!
     @IBOutlet weak var SectionFAQ: UIView! // ? , hide
     @IBOutlet weak var SectionSubtitle: UILabel! // ?
     
     // 40, 60
+    override func awakeFromNib() {
+        self.SectionImage.tint = true
+        self.SectionImage.tintColor = self.SectionTitle.textColor
+        
+        self.SectionSubtitle.text = ""
+    }
     
+    func adapt(_ image: String, title: String, subtitle: String?, isFaq: Bool) {
+        self.SectionImage.image = UIImage(named: image)!
+        self.SectionTitle.text = title
+        self.SectionSubtitle.text = subtitle
+        
+        self.SectionFAQ.isHidden = !isFaq
+    }
+    
+    static func heightFor(_ isSubtitle: Bool) -> CGFloat {
+        if isSubtitle {
+            return 60
+        }
+        return 40
+    }
 }
 
 // MARK: - Images Preview Cell
@@ -52,29 +72,71 @@ class AddProduct3ImagesPreviewCellNewOneCell: UICollectionViewCell {
 
 // MARK: - Detail Product Cell
 class AddProduct3DetailProductCell: UITableViewCell {
+    @IBOutlet weak var txtProductName: UITextField!
+    @IBOutlet weak var lblCategory: UILabel!
+    @IBOutlet weak var lblMerk: UILabel!
+    @IBOutlet weak var lblCondition: UILabel!
+    @IBOutlet weak var txtCacat: UITextField!
+    @IBOutlet weak var txtSpecialStory: UITextField!
+    @IBOutlet weak var txtAlasanJual: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
+    
+    
+    // 346 -> -40 // count description height
     
 }
 
 // MARK: - Weight Cell (Sell)
 class AddProduct3WeightCell: UITableViewCell {
+    @IBOutlet weak var vw1kg: BorderedView!
+    @IBOutlet weak var img1kg: TintedImageView!
+    @IBOutlet weak var lbl1kg: UILabel!
+    @IBOutlet weak var vw12kg: BorderedView!
+    @IBOutlet weak var img12kg: TintedImageView!
+    @IBOutlet weak var lbl12kg: UILabel!
+    @IBOutlet weak var vw2kg: BorderedView!
+    @IBOutlet weak var img2kg: TintedImageView!
+    @IBOutlet weak var lbl2kg: UILabel!
     
+    
+    // 72
 }
 
 // MARK: - Postal Fee Cell (Sell)
-class AddProduct3PostalCell: UITableViewCell {
+class AddProduct3PostalFeeCell: UITableViewCell {
+    @IBOutlet weak var vwFreeOngkir: BorderedView!
+    @IBOutlet weak var imgFreeOngkir: TintedImageView!
+    @IBOutlet weak var lblFreeOngkir: UILabel!
+    @IBOutlet weak var vwPaidOngkir: BorderedView!
+    @IBOutlet weak var imgPaidOngkir: TintedImageView!
+    @IBOutlet weak var lblPaidOngkir: UILabel!
+    @IBOutlet weak var lblRegion: UILabel!
     
+    // 206, count teks height
 }
 
 // MARK: - Product Auth Verification Cell (Luxury)
 class AddProduct3ProductAuthVerificationCell: UITableViewCell {
+    @IBOutlet weak var txtStyleName: UITextField!
+    @IBOutlet weak var txtSerialNumber: UITextField!
+    @IBOutlet weak var txtLokasiBeli: UITextField!
+    @IBOutlet weak var txtTahunBeli: UITextField!
     
+    // 172
 }
 
 // MARK: - Images Checklist Cell
 class AddProduct3ImagesChecklistCell: UITableViewCell {
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    
+    // 66, count height collection view (20 x total/y), count teks height
 }
 
 class AddProduct3ImagesChecklistCellCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblCheck: UILabel! // tosca
     
+    
+    // 120 x 20
 }
