df5c5f48c72bbeb06e1d2d77296165fcedab3b3d
Chyntia Leonie Andreas <chyn_leo038@yahoo.co.id>
2017-09-14 14:57:22 +0700

    version 1.8.2 build 3

--- Prelo/AppDelegate.swift
+++ Prelo/AppDelegate.swift
@@ -71,6 +71,123 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        
+        print("can print")
+        
+        if(launchedBefore){
+            print("has launched before")
+        } else {
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            // API Migrasi
+            let _ = request(APIApp.metadata(brands: "0", categories: "1", categorySizes: "0", shippings: "1", productConditions: "1", provincesRegions: "1")).responseJSON {resp in
+                if (PreloEndpoints.validate(false, dataResp: resp, reqAlias: "Reload App Data")) {
+                    let metaJson = JSON(resp.result.value!)
+                    let metadata = metaJson["_data"]
+                    
+                    var isSuccess : Bool = true
+                    let queue : OperationQueue = OperationQueue()
+                    
+                    let opCategories : Operation = BlockOperation(block: {
+                        let psc = UIApplication.appDelegate.persistentStoreCoordinator
+                        let moc = NSManagedObjectContext.init(concurrencyType: NSManagedObjectContextConcurrencyType.privateQueueConcurrencyType)
+                        moc.persistentStoreCoordinator = psc
+                        
+                        // Update categories
+                        //print("Updating categories..")
+                        if (CDCategory.deleteAll(moc)) {
+                            if (CDCategory.saveCategories(metadata["categories"], m: moc)) {
+                                DispatchQueue.main.async(execute: {
+                                    
+                                })
+                            } else {
+                                isSuccess = false
+                            }
+                        }
+                    })
+                    queue.addOperation(opCategories)
+                    
+                    
+                    let opShippings : Operation = BlockOperation(block: {
+                        let psc = UIApplication.appDelegate.persistentStoreCoordinator
+                        let moc = NSManagedObjectContext.init(concurrencyType: NSManagedObjectContextConcurrencyType.privateQueueConcurrencyType)
+                        moc.persistentStoreCoordinator = psc
+                        
+                        // Update shippings
+                        //print("Updating shippings..")
+                        if (CDShipping.deleteAll(moc)) {
+                            if (CDShipping.saveShippings(metadata["shippings"], m: moc)) {
+                                DispatchQueue.main.async(execute: {
+                                    
+                                })
+                            } else {
+                                isSuccess = false
+                            }
+                        }
+                    })
+                    queue.addOperation(opShippings)
+                    
+                    let opProductConditions : Operation = BlockOperation(block: {
+                        let psc = UIApplication.appDelegate.persistentStoreCoordinator
+                        let moc = NSManagedObjectContext.init(concurrencyType: NSManagedObjectContextConcurrencyType.privateQueueConcurrencyType)
+                        moc.persistentStoreCoordinator = psc
+                        
+                        // Update product conditions
+                        //print("Updating product conditions..")
+                        if (CDProductCondition.deleteAll(moc)) {
+                            if (CDProductCondition.saveProductConditions(metadata["product_conditions"], m: moc)) {
+                                DispatchQueue.main.async(execute: {
+                                    
+                                })
+                            } else {
+                                isSuccess = false
+                            }
+                        }
+                    })
+                    queue.addOperation(opProductConditions)
+                    
+                    let opProvincesRegions : Operation = BlockOperation(block: {
+                        let psc = UIApplication.appDelegate.persistentStoreCoordinator
+                        let moc = NSManagedObjectContext.init(concurrencyType: NSManagedObjectContextConcurrencyType.privateQueueConcurrencyType)
+                        moc.persistentStoreCoordinator = psc
+                        
+                        // Update provinces regions
+                        //print("Updating provinces regions..")
+                        if (CDProvince.deleteAll(moc) && CDRegion.deleteAll(moc)) {
+                            if (CDProvince.saveProvinceRegions(metadata["provinces_regions"], m: moc)) {
+                                DispatchQueue.main.async(execute: {
+                                   
+                                })
+                            } else {
+                                isSuccess = false
+                            }
+                        }
+                    })
+                    queue.addOperation(opProvincesRegions)
+                    
+                    let opFinish : Operation = BlockOperation(block: {
+                        //                    a.dismiss(withClickedButtonIndex: -1, animated: true)
+                        if (isSuccess) {
+                            DispatchQueue.main.async(execute: {
+                                
+                            })
+                        } else {
+                            DispatchQueue.main.async(execute: {
+                                
+                            })
+                        }
+                    })
+                    opFinish.addDependency(opCategories)
+                    opFinish.addDependency(opShippings)
+                    opFinish.addDependency(opProductConditions)
+                    opFinish.addDependency(opProvincesRegions)
+                    queue.addOperation(opFinish)
+                } else {
+                    
+                }
+            }
+        }
+        
         // alamofire-image fixer
         DataRequest.addAcceptableImageContentTypes(["image/jpg","binary/octet-stream"])
         
